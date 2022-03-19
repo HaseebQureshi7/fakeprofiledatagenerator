c7e30769d72511df4c5c4891c362b2dec3816f9f
Haseeb <qureshihaxeeb2@gmail.com>
2022-03-19 16:24:16 +0530

    minor fixes and additions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
   return (
     <>
     <Profile/>
-    <p className='nextbutton' onClick={repeater}>New Profile</p>
+    <p className='nextbutton' onClick={repeater}>New Profile {'>>'}</p>
+    <div className='atagholder'>
+    <a href='https://www.linkedin.com/in/haseebqureshiishere' className='credits'>Developers Profile  &lt; / &gt;</a>
+    </div>
     </>
   );
 }
